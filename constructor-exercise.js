aab7a21c10093f6d3efcebf4aeed7a8eea93de70
SmokyDaBear <jeshurungreen@gmail.com>
2025-07-13 18:38:06 -0500

    added connstructors and classes

--- constructor-exercise.js
+++ constructor-exercise.js
@@ -1,1 +1,35 @@
-/* Please read the instructions located in the 1-constructor-exercise-readme.md file and complete your code below */ 
+/* Please read the instructions located in the 1-constructor-exercise-readme.md file and complete your code below */
+function Book(title, author, year, genre) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.genre = genre;
+}
+const currentYear = 2025;
+Book.prototype.getSummary = function () {
+  return `${this.title} by ${this.author} is a ${this.genre} book published in ${this.year}.`;
+};
+Book.prototype.isClassic = function () {
+  return this.year < currentYear - 50;
+};
+Book.prototype.updateYear = function (newYear) {
+  this.year = newYear;
+  return `Updated ${this.title}'s publishing year to ${this.year}`;
+};
+Book.prototype.changeGenre = function (newGenre) {
+  this.genre = newGenre;
+  return `${this.title}'s genre updated to ${this.genre}`;
+};
+
+const goodBook = new Book(
+  "To Kill a Mockingbird",
+  "Harper Lee",
+  1960,
+  "Fiction"
+);
+console.log(goodBook);
+console.log(goodBook.getSummary());
+console.log(`${goodBook.title} is a classic: ${goodBook.isClassic()}`);
+console.log(goodBook.updateYear(2000));
+console.log(goodBook.changeGenre("Historical Fiction"));
+console.log(goodBook.getSummary());
--- oop-exercise.js
+++ oop-exercise.js
@@ -1,1 +1,90 @@
 /* Please read the instructions located in the exercises/2-oop-exercise-readme.md file and complete your code below */
+class Student {
+  constructor(name, age, grades) {
+    this.name = name;
+    this.age = age;
+    this.grades = grades;
+  }
+  addGrade(newGrade) {
+    newGrade <= 100 && newGrade >= 0
+      ? this.grades.push(newGrade)
+      : console.error(`\n${newGrade} must be between 0 - 100`);
+  }
+  calcAvgGrade() {
+    return this.grades.reduce((acc, cur) => acc + cur, 0) / this.grades.length;
+  }
+  hasPassed(passingGrade) {
+    return this.calcAvgGrade() >= passingGrade;
+  }
+  getAge() {
+    return this.age;
+  }
+  getName() {
+    return this.name;
+  }
+}
+
+//Test Student class constructor and methods:
+const studentObjTest = new Student("testie", 18, [100, 0, 24, 89]);
+console.log(studentObjTest.grades);
+studentObjTest.addGrade(37);
+console.log(studentObjTest.grades);
+console.log(
+  studentObjTest.calcAvgGrade(),
+  studentObjTest.hasPassed(50),
+  studentObjTest.getAge(),
+  studentObjTest.getName()
+);
+
+class Course {
+  constructor(title, students, alumni) {
+    this.title = title;
+    this.students = students;
+    this.alumni = alumni;
+  }
+  enrollStudent(newStudent) {
+    if (!this.students) {
+      this.students = [];
+    }
+    this.students.push(newStudent);
+  }
+  graduateStudent(studentName) {
+    if (!this.alumni) {
+      this.alumni = [];
+    }
+    this.alumni.push(
+      this.students.find((student) => student.name === studentName)
+    );
+    this.students = this.students.filter(
+      (student) => student.name != studentName
+    );
+  }
+  listStudents() {
+    return this.students.map((student) => student.name);
+  }
+  calcClassAvgGrade() {
+    return this.students
+      ? this.students.reduce(
+          (acc, student) => acc + student.calcAvgGrade(),
+          0
+        ) / this.students.length
+      : 0;
+  }
+}
+//test out the Course constructor and methods:
+const student1 = new Student("John Doe", 24, [27, 89, 57]);
+const student2 = new Student("Rachel Ray", 21, [92, 99, 89, 91]);
+
+const algebra1 = new Course("Algebra I", [student1, student2]);
+console.log(algebra1);
+console.log(algebra1.calcClassAvgGrade());
+
+const algebra2 = new Course("Algebra II");
+console.log(algebra2.calcClassAvgGrade());
+algebra2.enrollStudent(student1);
+algebra2.enrollStudent(student2);
+console.log(algebra2.calcClassAvgGrade());
+console.log(algebra2.listStudents());
+algebra1.graduateStudent("John Doe");
+console.log(algebra1.listStudents());
+console.log(algebra1.alumni);
